ba1ee3b772b4632e48fa2c1695c3d6b53893157f
avpash43 <avpash43@gmail.com>
2019-06-26 21:56:10 +0300

    Add abonent_view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,3 +18,33 @@ insert into PhoneNumbers (external, internal, personal)
 insert into Abonent (employee, position, departament, address, phonenumber, headofunit, wasadded)
                 values ('1', '1', '1', '1', '1', '1', '2019-06-24 13:00:00');
 
+create view ABONENT_VIEW as (
+select
+    ab.id as id,
+    emp.firstname as firstname,
+    emp.lastname as lastname,
+    pos.position as position,
+    dep.departament as departament,
+    ad.city as city,
+    ad.street as street,
+    ad.build as build,
+    phn.external as external_number,
+    phn.internal as internal_number,
+    phn.personal as personal_number,
+    hou.lastname as headofunit_lasname,
+    hou.firstname as headofunit_firstname,
+    ab.wasadded as wasadded
+from abonent as ab
+    left join address as ad
+        on ad.id=ab.address
+    left join departament as dep
+        on dep.id=ab.departament
+    left join employee as emp
+        on emp.id=ab.employee
+    left join phonenumbers as phn
+        on phn.id=ab.phonenumber
+    left join employee as hou
+        on hou.id=ab.headofunit
+    left join position as pos
+        on pos.id=ab.position
+)
