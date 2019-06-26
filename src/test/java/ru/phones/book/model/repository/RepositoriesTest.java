67b0922f11a8e23607a80bccd047562b04dcbef5
Aleksandr_Pashkovskii <aleksandr_pashkovskii@epam.com>
2019-06-26 16:54:05 +0300

    Add repository tests

--- src/test/java/ru/phones/book/model/repository/RepositoriesTest.java
+++ src/test/java/ru/phones/book/model/repository/RepositoriesTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.phones.book.model.repositories.*;
 
@@ -14,13 +13,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class RepositoriesTest {
-    @Autowired
-    private EmployeeRepository employeeRepository;
-    private AbonentRepository abonentRepository;
-    private AddressRepository addressRepository;
-    private DepartamentRepository departamentRepository;
-    private PhoneNumbersRepository phoneNumbersRepository;
-    private PositionRepository positionRepository;
+
+    @Autowired private EmployeeRepository employeeRepository;
+    @Autowired private AbonentRepository abonentRepository;
+    @Autowired private AddressRepository addressRepository;
+    @Autowired private DepartamentRepository departamentRepository;
+    @Autowired private PhoneNumbersRepository phoneNumbersRepository;
+    @Autowired private PositionRepository positionRepository;
 
     @Test
     public void injectedEmployeeAreNotNull(){
@@ -28,27 +27,27 @@ public class RepositoriesTest {
     }
 
     @Test
-    public void injectedAdressAreNotNull(){
-//        assertThat(addressRepository).isNotNull();
+    public void injectedAddressAreNotNull(){
+        assertThat(addressRepository).isNotNull();
     }
 
     @Test
     public void injectedAbonentAreNotNull(){
-//        assertThat(abonentRepository).isNotNull();
+        assertThat(abonentRepository).isNotNull();
     }
 
     @Test
     public void injectedDepartamentAreNotNull(){
-//        assertThat(departamentRepository).isNotNull();
+        assertThat(departamentRepository).isNotNull();
     }
 
     @Test
     public void injectedPhoneNumbersAreNotNull(){
-//        assertThat(phoneNumbersRepository).isNotNull();
+        assertThat(phoneNumbersRepository).isNotNull();
     }
 
     @Test
     public void injectedPositionAreNotNull(){
-//        assertThat(positionRepository).isNotNull();
+        assertThat(positionRepository).isNotNull();
     }
 }
