52b876e631c906fcbdaa55d26595335bd3d89e03
Aleksandr_Pashkovskii <aleksandr_pashkovskii@epam.com>
2019-06-27 13:34:13 +0300

    refactor tests

--- src/test/java/ru/phones/book/model/EmployeeServiceImplTests.java
+++ src/test/java/ru/phones/book/model/EmployeeServiceImplTests.java
@@ -6,24 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.phones.book.model.entites.Employee;
-import ru.phones.book.model.repositories.EmployeeRepository;
+import ru.phones.book.model.implementations.EmployeeServiceImpl;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EmployeeRepositoryTests {
+public class EmployeeServiceImplTests {
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeServiceImpl employeeServiceImpl;
 
     @Test
     public void testSaveEmployee() {
         Employee employee = new Employee();
         employee.setFirstname("test");
         employee.setLastname("testov");
-        employeeRepository.save(employee);
-        Employee employee2 = employeeRepository.findByLastname("testov").get();
+        employeeServiceImpl.addEmployee(employee);
+        Employee employee2 = employeeServiceImpl.findByLastname("testov").getBody();
         assertNotNull(employee2);
         assertEquals(employee2.getFirstname(), employee.getFirstname());
         assertEquals(employee2.getLastname(), employee.getLastname());
@@ -34,13 +35,15 @@ public class EmployeeRepositoryTests {
         Employee employee = new Employee();
         employee.setFirstname("test2");
         employee.setLastname("testov2");
-        Long employeeId = employeeRepository.save(employee).getId();
+        Long employeeId = employeeServiceImpl.addEmployee(employee).getId();
+
         Employee employee2 = new Employee();
         employee2.setId(employeeId);
         employee2.setFirstname("test3");
         employee2.setLastname("testov3");
-        employeeRepository.save(employee2);
-        Employee employee1Updated = employeeRepository.findById(employeeId).get();
+        employeeServiceImpl.addEmployee(employee2);
+
+        Employee employee1Updated = employeeServiceImpl.findById(employeeId).getBody();
         assertNotNull(employee1Updated);
         assertEquals(employee2.getFirstname(), employee1Updated.getFirstname());
         assertEquals(employee2.getLastname(), employee1Updated.getLastname());
@@ -51,7 +54,9 @@ public class EmployeeRepositoryTests {
         Employee employee = new Employee();
         employee.setFirstname("test4");
         employee.setLastname("testov4");
-        employeeRepository.save(employee);
-        employeeRepository.delete(employee);
+        Long employeeId = employeeServiceImpl.addEmployee(employee).getId();
+        employeeServiceImpl.deleteEmployeeById(employeeId);
+        Employee deleted = employeeServiceImpl.findById(employeeId).getBody();
+        assertNull(deleted);
     }
 }
