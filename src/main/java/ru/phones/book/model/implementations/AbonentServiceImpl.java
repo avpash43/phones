160545097bbbfc1c891998e471560a0a6b662dac
Aleksandr_Pashkovskii <aleksandr_pashkovskii@epam.com>
2019-06-27 09:40:17 +0300

    AbonentService refactor

--- src/main/java/ru/phones/book/model/implementations/AbonentServiceImpl.java
+++ src/main/java/ru/phones/book/model/implementations/AbonentServiceImpl.java
@@ -1,29 +1,17 @@
 package ru.phones.book.model.implementations;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.phones.book.model.entites.Abonent;
 import ru.phones.book.model.repositories.AbonentRepository;
 import ru.phones.book.model.services.AbonentService;
 
-import java.util.List;
 
 @Service
 public class AbonentServiceImpl implements AbonentService {
 
     @Autowired
     private AbonentRepository abonentRepository;
-
-    @Override
-    public List<Abonent> findAll() {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Abonent> findById(Long abonentId) {
-        return null;
-    }
 
     @Override
     public Abonent addAbonent(Abonent abonent) {
--- src/main/java/ru/phones/book/model/services/AbonentService.java
+++ src/main/java/ru/phones/book/model/services/AbonentService.java
@@ -1,13 +1,9 @@
 package ru.phones.book.model.services;
 
-import org.springframework.http.ResponseEntity;
 import ru.phones.book.model.entites.Abonent;
 
-import java.util.List;
 
 public interface AbonentService {
-    List<Abonent> findAll();
-    ResponseEntity<Abonent> findById(Long abonentId);
     Abonent addAbonent(Abonent abonent);
     void deleteAbonentById(Long abonentId);
     void updateAbonent(Abonent abonent);
